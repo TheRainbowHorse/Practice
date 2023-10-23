d4e0714432043a710ed4c888e79d2efc8383906b
TheRainbowHorse <therainbowhorse3@gmail.com>
2023-10-23 21:50:08 +0300

    block 2 day 4

---
+++ src/components/StudentInfo.vue
@@ -0,0 +1,36 @@
+<template>
+<table>
+    <tr>
+        <td><img :src="student.photo"></td>
+    </tr>
+    <tr>
+        <td>{{student.name}}</td>
+        <td>{{student.group}}</td>
+        <td>{{student.mark}}</td>
+        <td>{{student.isDonePr}}</td>
+    </tr>
+</table>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+export default {
+    props: {
+        id: ''
+    },
+    setup(props) {
+        const student = ref({});
+
+        onMounted(() => {
+            axios.get('http://34.82.81.113:3000/students/'+props.id).then((response) => {
+                console.log(response.data)
+                student.value = response.data;
+            })
+        });
+
+        return { student };
+    },
+}
+</script>
